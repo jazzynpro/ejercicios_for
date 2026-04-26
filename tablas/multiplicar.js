1cf78eee8234867b79d130f886133073f32fd00c
jazzynpro <reader_jaz@hotmail.com>
2026-04-25 23:47:31 -0500

    tablas de multiplicar

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,8 +1,11 @@
 function generarTablas(){
     let contenedor = document.getElementById("divTablas");
     let contenido = "";
+    let numero= document.getElementById("inputNumero").value;
+    let numeroInt=parseInt(numero);
+
     for(let i=1; i<=10; i++){
-        contenido=contenido+"<div class='fila'> 5 x " +i+ " = " +(5*i) + "</div>" ;
+        contenido=contenido+"<div class='fila'>" +numeroInt+ " x " +i+ " = " +(numeroInt*i) + "</div>" ;
     }
     contenedor.innerHTML = contenido;
 }
