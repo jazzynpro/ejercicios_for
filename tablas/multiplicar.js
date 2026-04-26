a0a918f07be708ddd08e18cfd10f04bd0b404888
jazzynpro <reader_jaz@hotmail.com>
2026-04-26 00:09:50 -0500

    Taller Multiplicacion

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,11 +1,32 @@
 function generarTablas(){
     let contenedor = document.getElementById("divTablas");
+    let mensaje = document.getElementById("mensaje");
     let contenido = "";
-    let numero= document.getElementById("inputNumero").value;
-    let numeroInt=parseInt(numero);
+    let numero = document.getElementById("inputNumero").value;
 
-    for(let i=1; i<=10; i++){
-        contenido=contenido+"<div class='fila'>" +numeroInt+ " x " +i+ " = " +(numeroInt*i) + "</div>" ;
+    // validar vacío
+    if(numero === ""){
+        mensaje.textContent = "⚠️ Por favor escribe un número";
+        mensaje.className = "error";
+        contenedor.innerHTML = "";
+        return;
     }
+
+    // validar número válido
+    if(numero <= 0){
+        mensaje.textContent = "⚠️ Ingresa un número mayor a 0";
+        mensaje.className = "error";
+        contenedor.innerHTML = "";
+        return;
+    }
+
+    // mensaje correcto
+    mensaje.textContent = "✅ Mostrando tabla del " + numero;
+    mensaje.className = "ok";
+
+    for(let i = 1; i <= 10; i++){
+        contenido += `<div class="fila">${numero} x ${i} = ${numero * i}</div>`;
+    }
+
     contenedor.innerHTML = contenido;
 }
